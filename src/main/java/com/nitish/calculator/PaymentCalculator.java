5860fb83c94566112d6600736e4d6b7abefab8f4
Nitish Shukla <nitish.shukla@crest.in>
2016-05-17 20:36:32 +0530

    happy path for default charge b/w start to bed

--- src/main/java/com/nitish/calculator/PaymentCalculator.java
+++ src/main/java/com/nitish/calculator/PaymentCalculator.java
@@ -2,9 +2,18 @@ package com.nitish.calculator;
 
 import com.nitish.builder.WorkingHour;
 
+import static com.nitish.charge.ChargeEnum.START_TIME_TO_BED_TIME_CHARGE;
+import static java.lang.Integer.valueOf;
+
 public class PaymentCalculator {
 
-    public Integer calculate(WorkingHour workingHour) {
-        return 1;
+    private static final String REGEX = ":";
+
+    public int calculate(WorkingHour workingHour) {
+        String startTime = workingHour.getStartTime();
+        String[] startTimeToken = startTime.split(REGEX);
+        String bedTime = workingHour.getBedTime();
+        String[] bedTimeToken = bedTime.split(REGEX);
+        return (valueOf(bedTimeToken[0]) - valueOf(startTimeToken[0])) * START_TIME_TO_BED_TIME_CHARGE.getAmount();
     }
 }
--- src/test/java/com/nitish/calculator/PaymentCalculatorTest.java
+++ src/test/java/com/nitish/calculator/PaymentCalculatorTest.java
@@ -3,16 +3,18 @@ package com.nitish.calculator;
 import com.nitish.builder.WorkingHour;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 public class PaymentCalculatorTest {
 
+    private static final int DEFAULT_CHARGE_BETWEEN_START_TO_BED_TIME = 48;
+
     @Test
-    public void shouldCalculateCharge() {
+    public void shouldCalculateChargeFromStartTimeToBedTime() {
         PaymentCalculator calculator = new PaymentCalculator();
 
-        Integer amount = calculator.calculate(WorkingHour.builder().build());
+        int amount = calculator.calculate(WorkingHour.builder().build());
 
-        assertNotNull(amount);
+        assertEquals(DEFAULT_CHARGE_BETWEEN_START_TO_BED_TIME, amount);
     }
 }
