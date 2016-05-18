00abb8db77cab837a10e92bb4fc72f75054aab53
Nitish Shukla <nitish.shukla@crest.in>
2016-05-18 14:55:37 +0530

    start time boundary test 17 hours added

--- src/main/java/com/nitish/validator/TimeValidator.java
+++ src/main/java/com/nitish/validator/TimeValidator.java
@@ -6,10 +6,11 @@ import static com.nitish.calculator.RegexExpression.REGEX;
 import static java.lang.Integer.valueOf;
 
 public class TimeValidator {
+
     public Boolean isStartEndTimeAreValid(WorkingHour workingHour) {
         String[] startTimeToken = workingHour.getStartTime().split(REGEX);
         String[] endTimeToken = workingHour.getEndTime().split(REGEX);
-        Boolean isValid = false;
+        Boolean isValid = true;
         if (valueOf(startTimeToken[0]) < 17) {
             isValid = false;
             return isValid;
--- src/test/java/com/nitish/validator/TimeValidatorTest.java
+++ src/test/java/com/nitish/validator/TimeValidatorTest.java
@@ -4,15 +4,26 @@ import com.nitish.builder.WorkingHour;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TimeValidatorTest {
 
     @Test
-    public void shouldValidatedStartAndEndTimeBeCorrect() {
+    public void shouldReturnFalseWhenStartTimeIsLessThen17Hours() {
         TimeValidator timeValidator = new TimeValidator();
 
-        Boolean isValid = timeValidator.isStartEndTimeAreValid(WorkingHour.builder().withStartTime("4:00PM").withEndTime("4:30AM").build());
+        Boolean isValid = timeValidator.isStartEndTimeAreValid(WorkingHour.builder().withStartTime("4:00PM").build());
 
         assertFalse(isValid);
+    }
+
+    @Test
+    public void shouldReturnTrueWhenStartTimeIs17Hours() {
+        TimeValidator timeValidator = new TimeValidator();
+
+        Boolean isValid = timeValidator.isStartEndTimeAreValid(WorkingHour.builder().withStartTime("5:00PM").build());
+
+        assertTrue(isValid);
+
     }
 }
